655cb66800a33ac8d6a7870446f104f2cd65276b
Luke Williams <lukewilliams10101@gmail.com>
2023-10-06 14:34:30 +0200

    feat: service logic added

--- spring-sec-prac/src/main/java/com/example/springsecprac/service/UserService.java
+++ spring-sec-prac/src/main/java/com/example/springsecprac/service/UserService.java
@@ -1,2 +1,26 @@
-package com.example.springsecprac.service;public class UserService {
+package com.example.springsecprac.service;
+
+import com.example.springsecprac.model.UserDetails;
+import com.example.springsecprac.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+    private final UserRepository repo;
+
+    public UserService(UserRepository repo) {
+        this.repo = repo;
+    }
+
+    public UserDetails createUser(OidcUser user) {
+        return repo.createUser(user);
+    }
+
+    public Optional<UserDetails> getUserBySubject(String userSubject) {
+        return repo.getUserBySubject(userSubject);
+    }
 }
