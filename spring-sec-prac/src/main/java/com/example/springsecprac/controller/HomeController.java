8b90dfeb96586d23d0749f68f2c2e666ae532eb6
Luke Williams <lukewilliams10101@gmail.com>
2023-10-06 14:34:45 +0200

    feat: controller logic added

--- spring-sec-prac/src/main/java/com/example/springsecprac/controller/HomeController.java
+++ spring-sec-prac/src/main/java/com/example/springsecprac/controller/HomeController.java
@@ -1,2 +1,38 @@
-package com.example.springsecprac.controller;public class HomeController {
+package com.example.springsecprac.controller;
+
+import com.example.springsecprac.model.UserDetails;
+import com.example.springsecprac.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HomeController {
+
+    @Autowired
+    private OAuth2AuthorizedClientService authorizedClientService;
+
+    @Autowired
+    UserService userService;
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello, home!";
+    }
+
+    @GetMapping("/secured")
+    public String update() {
+        return "Hello! Only authorized users can see this";
+    }
+
+    @GetMapping("user")
+    public ResponseEntity<UserDetails> getUser(@AuthenticationPrincipal OidcUser user) {
+        var currentUser = userService.getUserBySubject(user.getSubject());
+        return ResponseEntity.of(currentUser);
+    }
 }
