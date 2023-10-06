47a9bd96eabaf0ec1a7cde865deaa152a3c4bb76
Luke Williams <lukewilliams10101@gmail.com>
2023-10-06 14:33:16 +0200

    feat: repository implemented

--- spring-sec-prac/src/main/java/com/example/springsecprac/repository/UserJPARepository.java
+++ spring-sec-prac/src/main/java/com/example/springsecprac/repository/UserJPARepository.java
@@ -1,2 +1,11 @@
-package com.example.springsecprac.repository;public interface UserJPARepository {
+package com.example.springsecprac.repository;
+
+import com.example.springsecprac.model.UserDetails;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface UserJPARepository extends CrudRepository<UserDetails, Long> {
+
+    Optional<UserDetails> findBySubject(String userSubject);
 }
--- spring-sec-prac/src/main/java/com/example/springsecprac/repository/UserRepository.java
+++ spring-sec-prac/src/main/java/com/example/springsecprac/repository/UserRepository.java
@@ -1,2 +1,32 @@
-package com.example.springsecprac.repository;public class UserRepository {
+package com.example.springsecprac.repository;
+
+import com.example.springsecprac.model.UserDetails;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public class UserRepository {
+
+    private final UserJPARepository userJPARepository;
+
+    public UserRepository(UserJPARepository userJPARepository) {
+        this.userJPARepository = userJPARepository;
+    }
+
+    public UserDetails createUser(OidcUser userToSave) {
+        UserDetails user = new UserDetails(
+                userToSave.getSubject(),
+                userToSave.getGivenName(),
+                userToSave.getFamilyName(),
+                userToSave.getEmail(),
+                userToSave.getPicture()
+        );
+        return userJPARepository.save(user);
+    }
+
+    public Optional<UserDetails> getUserBySubject(String userSubject) {
+        return userJPARepository.findBySubject(userSubject);
+    }
 }
